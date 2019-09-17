38bbec82c0e493d8c9df7169290a446e805e1748
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-09-17 19:24:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,4 +4,5 @@ def reverse_each_word(sentence)
   array.each_index {|i| reverse_array[i] = array[i].reverse}
   reverse_sentence = ""
   reverse_array.each_index {|i| reverse_sentence << (array[i] + " ")}
+  reverse_sentence
 end
